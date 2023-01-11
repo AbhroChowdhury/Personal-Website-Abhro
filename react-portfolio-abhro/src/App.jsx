b4819f164277178133ef9dcd6c02a4fb8fbb0eed
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-10 20:49:58 -0700

    updating app.jsx

--- react-portfolio-abhro/src/App.jsx
+++ react-portfolio-abhro/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import References from './components/references/References'
 
 const app = () => {
   return (
-    <div>app</div>
+    <>
+        <Header /> 
+        <Navbar />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <References />
+
+
+    </>
   )
 }
 
