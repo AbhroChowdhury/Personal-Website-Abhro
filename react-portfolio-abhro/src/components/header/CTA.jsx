584a5bf6c49ab1f8c86f5b8e85ab4188caaacd1b
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-11 15:41:23 -0700

    finsihing up header

--- react-portfolio-abhro/src/components/header/CTA.jsx
+++ react-portfolio-abhro/src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../graphics/AbhroChowdhuryResumeJan09_COOP.pdf'
 const CTA = () => {
   return (
     <div className="CTA">
-        <a href={Resume} download className='btn'>Download my resume</a>
+        <a href={Resume} download className='btn'>Download CV</a>
         <a href="#contact" className='btn'>Contact Me </a>
     </div>
   )
