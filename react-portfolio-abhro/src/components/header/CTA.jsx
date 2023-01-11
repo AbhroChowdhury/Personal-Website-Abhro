fb9bfe698c9f41e7c00c2649b765c7e44bf19f5f
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-10 23:04:55 -0700

    updates to main page

---
+++ react-portfolio-abhro/src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resume from '../../graphics/AbhroChowdhuryResumeJan09_COOP.pdf'
+
+const CTA = () => {
+  return (
+    <div className="CTA">
+        <a href={Resume} download className='btn'>Download my resume</a>
+        <a href="#contact" className='btn'>Contact Me </a>
+    </div>
+  )
+}
+
+export default CTA
--- react-portfolio-abhro/src/components/header/Header.jsx
+++ react-portfolio-abhro/src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>  
+      <div className="container header__container">
+        <h5>Hello, my name is</h5>
+        <h1>Abhro Chowdhury</h1>
+        <h5 className="text-light">Software Engineering Student at Ualberta</h5>
+        <CTA />
+
+      </div>
+    </header>
   )
 }
 
