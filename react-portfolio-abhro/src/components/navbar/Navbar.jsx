11e221f69d36dc37b5ce7d93de45f03677b743cc
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-11 18:13:16 -0700

    useState is not working idk why

--- react-portfolio-abhro/src/components/navbar/Navbar.jsx
+++ react-portfolio-abhro/src/components/navbar/Navbar.jsx
@@ -5,17 +5,22 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {MdOutlineWorkOutline} from 'react-icons/md'
 import {AiOutlineCreditCard} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
+import {useState} from 'react'
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#header"><AiOutlineHome/> </a>
-      <a href="#about"><AiOutlineUser/> </a>
-      <a href="#experience"><MdOutlineWorkOutline/> </a>
-      <a href="#services"><AiOutlineCreditCard/> </a>
-      <a href="#contact"><AiOutlineMail/> </a>
+      <a href="#" onclick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/> </a> 
+      <a href="#about" onclick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/> </a>
+      <a href="#experience" onclick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWorkOutline/> </a>
+      <a href="#services" onclick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><AiOutlineCreditCard/> </a>
+      <a href="#contact" onclick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMail/> </a>
     </nav>
   )
 }
 
 export default Navbar
+
+/* <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/> </a>
+basically says: if the active state is now #, then we want to set the classname to active */
