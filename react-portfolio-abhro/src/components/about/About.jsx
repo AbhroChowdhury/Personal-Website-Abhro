fd13f977271d2f81d7640ffb0819b84c4e727cfd
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-11 18:33:49 -0700

    starting on about component

--- react-portfolio-abhro/src/components/about/About.jsx
+++ react-portfolio-abhro/src/components/about/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './about.css'
+import ME from '../../graphics/abhropic2EIC.jpg'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5> Some more </h5>
+      <h2> About Me </h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me_image'>
+            <img src={ME} alt="About me image" />
+          </div> 
+
+        </div>
+      </div>
+    </section> 
   )
 }
 
