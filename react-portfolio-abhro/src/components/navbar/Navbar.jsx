0a7679a8a9c1a7fcb183d07c731031efcf5b25a2
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-11 17:38:21 -0700

    creating nav bar

--- react-portfolio-abhro/src/components/navbar/Navbar.jsx
+++ react-portfolio-abhro/src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdOutlineWorkOutline} from 'react-icons/md'
+import {AiOutlineCreditCard} from 'react-icons/ai'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href="#"><AiOutlineHome/> </a>
+      <a href="#about"><AiOutlineUser/> </a>
+      <a href="#experience"><MdOutlineWorkOutline/> </a>
+      <a href="#services"><AiOutlineCreditCard/> </a>
+      <a href="#contact"><AiOutlineMail/> </a>
+    </nav>
   )
 }
 
