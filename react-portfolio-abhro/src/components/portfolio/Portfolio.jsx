3e7623492ac8dcc9550f4f0bdd9d4241ae4c16b7
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-12 16:41:33 -0700

    updates on portfolio

--- react-portfolio-abhro/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-abhro/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../graphics/thumbnailHACKED.png'
+import IMG2 from '../../graphics/receiptPHOTO.png'
+import IMG3 from '../../graphics/dumbells.webp'
+import IMG4 from '../../graphics/mywebsite.png'
+import IMG5 from '../../graphics/opencv1.jpeg'
+import IMG6 from '../../graphics/speech.png'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'> 
+      <h2>MY PORTFOLIO</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__items'>
+          <div className='portfolio__image'>
+            <img src={IMG1} alt=""/>
+          </div>
+          <h3>
+            portfolio item title
+          </h3>
+          <a href="https://github.com/AbhroChowdhury/Company-Stocker" className='btn'></a>
+        </article>
+      </div>
+
+
+
 
     </section>
   )
