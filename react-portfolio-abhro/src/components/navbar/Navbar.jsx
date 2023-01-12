fb33c153cf1c9a8d0fa68a36703a761d17ce57dc
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-12 16:05:45 -0700

    shifting stuff around

--- react-portfolio-abhro/src/components/navbar/Navbar.jsx
+++ react-portfolio-abhro/src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {MdOutlineWorkOutline} from 'react-icons/md'
-import {AiOutlineCreditCard} from 'react-icons/ai'
+import {AiOutlineBook} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
 import {useState} from 'react'
 
@@ -14,7 +14,7 @@ const Navbar = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/> </a> 
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/> </a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWorkOutline/> </a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineCreditCard/> </a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineBook/> </a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMail/> </a>
     </nav>
   )
