0116f87654646c130aaf81fc6d2fc78493628d6f
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-11 19:16:38 -0700

    progress on about me section

--- react-portfolio-abhro/src/components/about/About.jsx
+++ react-portfolio-abhro/src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../graphics/abhropic2EIC.jpg'
+import {FaLaptopCode} from 'react-icons/fa'
+import {TbSchool} from 'react-icons/tb'
+import {AiOutlineFund} from 'react-icons/ai'
 
 const About = () => {
   return (
@@ -18,11 +21,30 @@ const About = () => {
         <div className='about__content'>
           <div className='about__cards'>
             <article className='about__card'>
-              <
+              <FaLaptopCode className="about__icon"/>
               <h5>Experience</h5>
               <small>~9 months professional experience</small>
             </article>
+
+            <article className='about__card'>
+              <TbSchool className="about__icon"/>
+              <h5>Software Engineering</h5>
+              <small>University of Alberta</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineFund className="about__icon"/>
+              <h5>CO-OP Availability</h5>
+              <small>4 months / Starting May</small>
+            </article>
+
           </div>
+
+          <p>
+            FILL THIS IN LATER
+          </p>
+
+          <a href="#contact" className='btn'>Reach out to me</a>
         </div>
       </div>
     </section> 
