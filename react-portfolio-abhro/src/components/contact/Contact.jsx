3a105a5c9a954c80f7241dd3008529b27b864956
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-12 19:37:10 -0700

    almost done contact section

--- react-portfolio-abhro/src/components/contact/Contact.jsx
+++ react-portfolio-abhro/src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {FiGithub} from 'react-icons/fi'
 
 const Contact = () => {
   return (
@@ -10,13 +12,33 @@ const Contact = () => {
         <div className="container contact__container"> 
           <div className="contact__options">
             <article className='contact__option'>
-              <AiOutlineMail/>
+              <AiOutlineMail className='contact-icon'/>
               <h4>Email</h4>
               <h5>abhrajyo@ualberta.ca</h5>
-              <a href="mailto: abhrajyo@ualberta.ca">Email me</a>
+              <a href="mailto: abhrajyo@ualberta.ca" target='blank'>Email me</a>
             </article>
+
+            <article className='contact__option'>
+              <AiOutlineLinkedin/>
+              <h4>LinkedIn</h4>
+              <h5>Abhro Chowdhury</h5>
+              <a href="https://www.linkedin.com/in/abhro-chowdhury-29a269203/" target='_blank'>Connect with me</a>
+            </article>
+
+            <article className='contact__option'>
+              <FiGithub/>
+              <h4>Github</h4>
+              <h5>AbhroChowdhury</h5>
+              <a href="https://github.com/AbhroChowdhury" target='_blank'>Collaborate with me</a>
+            </article>
+
           </div>
-          <form action=""></form>
+          <form action="">
+            <input type="text" name='name' placeholder='Enter your name' required />
+            <input type="text" name='Email' placeholder='Your Email required' />
+            <textarea name="message" rows="7" placeholder='Enter a message' required></textarea>
+            <a type='Submit' className='btn'>send message</a>
+          </form>
         </div>
 
 
